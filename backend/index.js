efddad35bf16b225e864ca3d5592697b4c74552f
Ulises-FG <11903377@upq.edu.mx>
2023-05-31 12:21:47 -0600

    estructura de endpoint, replicar en los demas

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,77 @@ const { PrismaClient } = require('@prisma/client');
 const app = express();
 const prisma = new PrismaClient();
 
-app.get('/users', async (req, res) => {
+app.use(express.json());
+
+app.post('/usuario', async (req, res) => {
+  console.log('req.body', req.body);
+  const body = req.body;
+  const { rol, nombre, contrasena, correo } = body;
   try {
-    const users = await prisma.user.findMany();
-    res.json(users);
+    const usuario = await prisma.user.create({
+      data: {
+        rol: rol,
+        nombre: nombre,
+        contrasena: contrasena,
+        correo: correo,
+      },
+    });
+    res.json(usuario);
   } catch (error) {
     res.status(500).json({ error: 'Error al obtener los usuarios' });
+  }
+});
+
+app.get('/usuario/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await prisma.user.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener el usuario' });
+  }
+});
+
+app.delete('/usuario/:id', async (req, res) => {
+  const idUsuario = parseInt(req.params.id);
+
+  try {
+    const usuario = await prisma.user.delete({
+      where: {
+        id: idUsuario,
+      },
+    });
+
+    res.json(usuario);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al eliminar el usuario' });
+  }
+});
+
+app.put('/usuario/:id', async (req, res) => {
+  const idUsuario = parseInt(req.params.id);
+  const { rol, nombre, contrasena, correo } = req.body;
+
+  try {
+    const usuario = await prisma.user.update({
+      where: {
+        id: idUsuario,
+      },
+      data: {
+        rol,
+        nombre,
+        contrasena,
+        correo,
+      },
+    });
+
+    res.json(usuario);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al actualizar el usuario' });
   }
 });
 
